697a101e4b8700a7eadecf4fd2e01847230d0eff
Melanie Gershman & Jung Choi <pair+melaniegershman.mr-jungchoi@devbootcamp.com>
2016-12-05 12:19:27 -0500

    Add logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
  def new
+
  end
 
  def create
+   @user = User.find_by_username(params[:username]).try(:authenticate, params[:password])
+   if @user
+     session[:user_id] = @user.id
+     redirect_to root_path
+   else
+     @errors = ["This username/password combination does not match."]
+     render 'sessions/new'
+   end
  end
 
  def destroy
+   session.clear
+   redirect_to root_path
  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create]
+  #  breaking convention here with the following line, because sessions are not conventional by nature
+  delete 'sessions' => 'sessions#destroy'
 
   root 'articles#index'
 end
