32b88711ce72845d286a19c6a4ae27994dcf7dee
Melanie Gershman & Jung Choi <pair+melaniegershman.mr-jungchoi@devbootcamp.com>
2016-12-05 19:17:36 -0500

    Add comment update and delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :find_article, only: [:show, :edit, :update, :destroy]
-  before_action :find_user, except: [:index]
+  before_action :find_user, except: :index
 
   def index
     @articles = Article.all
